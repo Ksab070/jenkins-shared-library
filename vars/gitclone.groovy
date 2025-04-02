29e857436fcb45cd7b73bed928e168b10fbb4ebb
Ksab070 <khansubhan070@gmail.com>
2025-04-02 13:45:37 +0530

    Added comments in gitclone

--- vars/gitclone.groovy
+++ vars/gitclone.groovy
@@ -1,5 +1,7 @@
 def call(String url, String branch) {
   git url: "${url}", branch: "${branch}"
+  //Take parameter during the build phase, perform git url on this
   echo "Code cloned Successfully"
-  sh "chmod +rwx ${workspace}/" //For changing the permission to be executable
+  sh "chmod +rwx ${workspace}/"
+  //For changing the permission to be executable
 }
